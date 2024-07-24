660d59fe09c64419347b20fc48f1a8a380d50683
StProger <kicode@mail.ru>
2024-07-24 15:30:50 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 def index():
 
-    ...
+    return HttpResponse("Главная страница")
 
-def group_posts():
 
-    ...
+def group_posts(slug: str):
+
+    return HttpResponse(f"Группа: {slug}")
