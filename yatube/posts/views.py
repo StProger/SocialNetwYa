38d110fd9d4694994c4f979ce337e4b4ea39acaf
StProger <kicode@mail.ru>
2024-07-27 01:17:29 +0300

    add models

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
+
 
 # Create your views here.
 def index(request):
 
     template = "posts/index.html"
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        "text": "Это главная страница проекта Yatube"
+        "text": "Это главная страница проекта Yatube",
+        "posts": posts
     }
     return render(request, template, context)
 
